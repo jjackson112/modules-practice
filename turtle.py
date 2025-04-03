c98d95b737ce529c8f13a3bec53d9da5d8c16619
jjackson112 <jjackson11211@gmail.com>
2025-04-03 12:23:48 -0400

    turtle

---
+++ turtle.py
@@ -0,0 +1,65 @@
+import turtle as turtle
+import random
+
+print(
+    "Welcome to your drawing app! Select a shape you want to draw. After typing the number and hitting enter, the drawing app will draw the shape you selected."
+)
+
+turtle.penup()
+turtle.left(90)
+turtle.forward(250)
+turtle.left(90)
+turtle.forward(300)
+turtle.pendown()
+
+turtle.write("Drawing App!", font=("Arial", 16, "normal"))
+
+turtle.penup()
+turtle.left(90)
+turtle.forward(50)
+turtle.pendown()
+
+turtle.write("To draw a shape, go to the Console tab and choose an option!",
+             font=("Arial", 16, "normal"))
+
+turtle.penup()
+turtle.forward(150)
+turtle.left(90)
+turtle.forward(150)
+turtle.pendown()
+
+
+def star():
+  # Star
+  for i in range(0, 5):
+    turtle.pencolor("blue")
+    turtle.forward(100)
+    turtle.right(144)
+
+
+def square():
+  # Square
+  for i in range(0, 4):
+    turtle.pencolor("green")
+    turtle.forward(100)
+    turtle.left(90)
+
+
+def hexagon():
+  # Hexagon
+  for i in range(0, 6):
+    turtle.pencolor("red")
+    turtle.forward(100)
+    turtle.left(60)
+
+
+selection = input("1. Star\n2. Square\n3. Hexagon\nSelect a number: ")
+if selection == "1":
+  print("Excellent choice! Go to the result tab to see your creation.")
+  star()
+elif selection == "2":
+  print("Excellent choice! Go to the result tab to see your creation.")
+  square()
+elif selection == "3":
+  print("Excellent choice! Go to the result tab to see your creation.")
+  hexagon()
